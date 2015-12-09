e6bfe5389cd7632f24a77f604ddce60bbe5e3b7e
Gerard <Gerard@.(none)>
2015-12-09 22:15:49 +0000

    Made a single executable file, but the classpath won't work with the embedded Scala presentation compiler. Thinking about next steps...

--- neptunium.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-java -jar "C:\Users\Gerard\OneDrive\Documents\Neptunium\target\scala-2.11\neptunium.jar" %1 %2
---
+++ neptuniumStub.cmd
@@ -0,0 +1,7 @@
+@echo off
+java -jar %~f0  %*
+exit /b
+REM Do not delete any newlines following this ... they will be required to keep the appended JAR file content distinct!
+
+
+
