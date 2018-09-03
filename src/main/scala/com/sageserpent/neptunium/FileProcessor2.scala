759be99b3c35f68ef389b9da5aa4c983caef0cee
Gerard Murphy <g.j.murphy@sageserpent.com>
2018-09-03 10:36:22 +0000

    Introduce case class hierarchy representing the YAML structure.

--- src/main/scala/com/sageserpent/neptunium/FileProcessor2.scala
+++ src/main/scala/com/sageserpent/neptunium/FileProcessor2.scala
@@ -3,19 +3,48 @@ import scala.meta.parsers.Parsed
 import scala.meta.Source
 
 object FileProcessor2 {
-  def discoverStructure(pathOfInputFile: String,
-                        charsetOfInputFile: String,
-                        pathOfOutputFileForYamlResult: String): Unit = ???
+  def discoverStructure(
+      pathOfInputFile: String,
+      charsetOfInputFile: String,
+      pathOfOutputFileForYamlResult: String
+  ): Unit = ???
 
-  case class File()
+  case class File(
+      `type`: String,
+      name: String,
+      locationSpan: LocationSpan,
+      footerSpan: Span,
+      children: Seq[Container],
+      parsingErrorsDetected: Boolean,
+      parsingError: Seq[ParsingError]
+  )
 
-  case class ParsingErrors()
+  case class ParsingError(location: LineAndOffSet, message: String)
 
   sealed trait Declaration
 
-  case class Container() extends Declaration
+  case class Container(
+      `type`: String,
+      name: String,
+      locationSpan: LocationSpan,
+      headerSpan: Span,
+      footerSpan: Span,
+      children: Seq[Container]
+  ) extends Declaration
 
-  case class Node() extends Declaration
+  case class Node(`type`: String, name: String, locationSpan: LocationSpan, span: Span) extends Declaration
+
+  type OneRelativeLineNumber = Int
+
+  type ZeroRelativeOffset = Int
+
+  type LineAndOffSet = (OneRelativeLineNumber, ZeroRelativeOffset)
+
+  type ZeroRelativeCharacterIndex = Int
+
+  case class LocationSpan(start: LineAndOffSet, end: LineAndOffSet)
+
+  type Span = (ZeroRelativeCharacterIndex, ZeroRelativeCharacterIndex)
 
   def structureOf(parsedSource: Parsed[Source]): File = ???
 }
--- src/test/scala/FileProcessor2Spec.scala
+++ src/test/scala/FileProcessor2Spec.scala
@@ -1,19 +1,86 @@
-import com.sageserpent.neptunium.FileProcessor2.File
+import com.sageserpent.neptunium.FileProcessor2._
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
 
-class FileProcessor2Spec extends FlatSpec with Matchers {
-  val aFileInstance = File()
+class FileProcessor2Spec extends FlatSpec with Matchers with MockFactory {
+  val aFile = mock[File]
 
-  "A file instance" should "have a type" in {
-    "aFileInstance.type: String" should compile
+  "A file" should "have a type" in {
+    "aFile.`type`: String" should compile
   }
 
-  it should "have a name" in {}
+  it should "have a name" in {
+    "aFile.name: String" should compile
+  }
 
-  it should "have a locationSpan" in {}
-  it should "have a footerSpan" in {}
+  it should "have a locationSpan" in {
+    "aFile.locationSpan: LocationSpan" should compile
+  }
 
-  it should "have optional children" in {}
+  it should "have a footerSpan" in {
+    "aFile.footerSpan: Span" should compile
+  }
 
-  it should "have optional parsing errors" in {}
+  it should "have optional children" in {
+    "aFile.children: Seq[Container]" should compile
+  }
+
+  it should "have optional parsing errors" in {
+    "aFile.parsingErrorsDetected: Boolean" should compile
+    "aFile.parsingError: Seq[ParsingError]" should compile
+  }
+
+  val aContainer = mock[Container]
+
+  "A container" should "have a type" in {
+    "aContainer.`type`: String" should compile
+  }
+
+  it should "have a name" in {
+    "aContainer.name: String" should compile
+  }
+
+  it should "have a locationSpan" in {
+    "aContainer.locationSpan: LocationSpan" should compile
+  }
+
+  it should "have a headerSpan" in {
+    "aContainer.headerSpan: Span" should compile
+  }
+
+  it should "have a footerSpan" in {
+    "aContainer.footerSpan: Span" should compile
+  }
+
+  it should "have optional children" in {
+    "aContainer.children: Seq[Declaration]" should compile
+  }
+
+  val aNode = mock[Node]
+
+  "A node" should "have a type" in {
+    "aNode.`type`: String" should compile
+  }
+
+  it should "have a name" in {
+    "aNode.name: String" should compile
+  }
+
+  it should "have a locationSpan" in {
+    "aNode.locationSpan: LocationSpan" should compile
+  }
+
+  it should "have a span" in {
+    "aNode.span: Span" should compile
+  }
+
+  val aParsingError = mock[ParsingError]
+
+  "A parsing error" should "have a location " in {
+    "aParsingError.location: LineAndOffSet" should compile
+  }
+
+  it should "have a message" in {
+    "aParsingError.message: String" should compile
+  }
 }
